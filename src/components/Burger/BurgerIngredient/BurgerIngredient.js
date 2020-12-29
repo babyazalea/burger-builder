a6a1f8a0f75a7939f253a8047074c46789f117fe
babyazalea <usun12@hotmail.com>
2020-12-29 13:03:34 +0900

    added prop-types & mapping burger-ingredients

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,6 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-import classes from "./BurgerIngerdient.module.css";
+import classes from "./BurgerIngredient.module.css";
 
 const burgerIngredient = (props) => {
   let ingredient = null;
@@ -12,8 +13,8 @@ const burgerIngredient = (props) => {
     case "bread-top":
       ingredient = (
         <div className={classes.BreadTop}>
-          <div className={classes.Seed1}></div>
-          <div className={classes.Seed2}></div>
+          <div className={classes.Seeds1}></div>
+          <div className={classes.Seeds2}></div>
         </div>
       );
       break;
@@ -33,6 +34,10 @@ const burgerIngredient = (props) => {
       ingredient = null;
   }
   return ingredient;
+};
+
+burgerIngredient.PropTypes = {
+  type: PropTypes.string.isRequired,
 };
 
 export default burgerIngredient;
