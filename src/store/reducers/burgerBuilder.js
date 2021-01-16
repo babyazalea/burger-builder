17def85eab15e117462c52cf11eef6f7096b39e0
babyazalea <usun12@hotmail.com>
2021-01-16 19:17:08 +0900

    redirecting checkout-page when burger building without user authentication

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -5,6 +5,7 @@ const initialState = {
   ingredients: null,
   totalPrice: 4,
   error: false,
+  building: false,
 };
 
 const INGREDIENT_PRICE = {
@@ -22,6 +23,7 @@ const addIngredient = (state, action) => {
   const updatedState = {
     ingredients: updatedIngredients,
     totalPrice: state.totalPrice + INGREDIENT_PRICE[action.ingredientName],
+    building: true,
   };
   return updateObject(state, updatedState);
 };
@@ -34,6 +36,7 @@ const removeIngredient = (state, action) => {
   const updatedState = {
     ingredients: updatedIngredients,
     totalPrice: state.totalPrice + INGREDIENT_PRICE[action.ingredientName],
+    building: true,
   };
   return updateObject(state, updatedState);
 };
@@ -48,6 +51,7 @@ const setIngredient = (state, action) => {
     },
     totalPrice: 4,
     error: false,
+    building: false,
   });
 };
 
