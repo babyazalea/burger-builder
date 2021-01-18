9586ec89d0db465d94707210b50b4833efefcaca
babyazalea <usun12@hotmail.com>
2021-01-18 14:12:53 +0900

    added using another test case using contains method

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -23,4 +23,11 @@ describe("<NavigationItems />", () => {
     wrapper.setProps({ isAuthenticated: true });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
+
+  it("should render Logout <NavigationItem /> elements if authentication", () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
+    ).toEqual(true);
+  });
 });
