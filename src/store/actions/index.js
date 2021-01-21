2aa96d6f63edfa6a436134c83ae96dacadf7758d
babyazalea <usun12@hotmail.com>
2021-01-21 22:03:16 +0900

    All the asynchronous codes in the application transferred to Redux-Saga

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,6 +2,28 @@ export {
   addIngredient,
   removeIngredient,
   initIngredients,
+  setIngredients,
+  fetchIngredientsFailed,
 } from "./burgerBuilder";
-export { purchaseBurger, purchaseInit, fetchOrders } from "./orders";
-export { auth, logout, setAuthRedirectPath, authCheckState } from "./auth";
+export {
+  purchaseBurger,
+  purchaseInit,
+  fetchOrders,
+  purchaseBurgerStart,
+  purchaseBurgerSuccess,
+  purchaseBurgerFail,
+  fetchOrderStart,
+  fetchOrdersSuccess,
+  fetchOrdersFail,
+} from "./orders";
+export {
+  auth,
+  logout,
+  setAuthRedirectPath,
+  authCheckState,
+  logoutSucceed,
+  authStart,
+  authSuccess,
+  authFail,
+  checkAuthTimeout,
+} from "./auth";
